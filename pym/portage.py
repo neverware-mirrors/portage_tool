268315b561a6fbbbe9e7fc1197acdac8d8c57da5
Zac Medico <zmedico@gentoo.org>
2006-11-14 14:07:52 +0000

    Fix a lock file leak. (trunk r5046)
    svn path=/main/branches/2.1.2/; revision=5047

--- pym/portage.py
+++ pym/portage.py
@@ -5064,7 +5064,7 @@ class portdbapi(dbapi):
 
 			mylock = None
 			try:
-				portage_locks.lockfile(mydbkey, wantnewlockfile=1)
+				mylock = portage_locks.lockfile(mydbkey, wantnewlockfile=1)
 				try:
 					os.unlink(mydbkey)
 				except (IOError, OSError), e:
