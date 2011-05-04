940a97e6582c470a935edb98ffd29abe853d3c88
David James <davidjames@chromium.org>
2011-05-04 10:46:17 -0700

    emerge: replace --rebuild with finer grained opts
    Replace --rebuild option with --rebuild-if-* options.
    
    --rebuild-if-new-rev [ y | n ]
      Rebuild packages when dependencies that are used at both build-time and
      run-time are built, if the dependency is not already installed with the
      same version and revision.
    
    --rebuild-if-new-ver [ y | n ]
      Rebuild packages when dependencies that are used at both build-time and
      run-time are built, if the dependency is not already installed with the
      same version. Revision numbers are ignored.
    
    --rebuild-if-unbuilt [ y | n ]
      Rebuild packages when dependencies that are used at both build-time and
      run-time are built.
    
    Change-Id: Ia50c1702bfe1b98a8d1891740e7bbb045921a905
    
    Review URL: http://gerrit.chromium.org/gerrit/280
    (cherry picked from commit e4297ca1075342ec89e089802cd25ae0817f3d5b)
    
    Conflicts:
    
    	pym/_emerge/depgraph.py

--- pym/portage/tests/resolver/test_rebuild.py
+++ pym/portage/tests/resolver/test_rebuild.py
@@ -12,6 +12,7 @@ class RebuildTestCase(TestCase):
 
 		ebuilds = {
 			"sys-libs/x-1": { },
+			"sys-libs/x-1-r1": { },
 			"sys-libs/x-2": { },
 			"sys-apps/a-1": { "DEPEND"  : "sys-libs/x", "RDEPEND" : "sys-libs/x"},
 			"sys-apps/a-2": { "DEPEND"  : "sys-libs/x", "RDEPEND" : "sys-libs/x"},
@@ -45,7 +46,7 @@ class RebuildTestCase(TestCase):
 		test_cases = (
 				ResolverPlaygroundTestCase(
 					["sys-libs/x"],
-					options = {"--rebuild" : True,
+					options = {"--rebuild-if-unbuilt" : True,
 						"--rebuild-exclude" : ["sys-apps/b"]},
 					mergelist = ['sys-libs/x-2', 'sys-apps/a-2', 'sys-apps/e-2'],
 					ignore_mergelist_order = True,
@@ -53,7 +54,7 @@ class RebuildTestCase(TestCase):
 
 				ResolverPlaygroundTestCase(
 					["sys-libs/x"],
-					options = {"--rebuild" : True},
+					options = {"--rebuild-if-unbuilt" : True},
 					mergelist = ['sys-libs/x-2', 'sys-apps/a-2', 'sys-apps/b-2',
 						'sys-apps/e-2', 'sys-apps/g-2'],
 					ignore_mergelist_order = True,
@@ -61,7 +62,7 @@ class RebuildTestCase(TestCase):
 
 				ResolverPlaygroundTestCase(
 					["sys-libs/x"],
-					options = {"--rebuild" : True,
+					options = {"--rebuild-if-unbuilt" : True,
 						"--rebuild-ignore" : ["sys-libs/x"]},
 					mergelist = ['sys-libs/x-2'],
 					ignore_mergelist_order = True,
@@ -69,13 +70,43 @@ class RebuildTestCase(TestCase):
 
 				ResolverPlaygroundTestCase(
 					["sys-libs/x"],
-					options = {"--rebuild" : True,
+					options = {"--rebuild-if-unbuilt" : True,
 						"--rebuild-ignore" : ["sys-apps/b"]},
 					mergelist = ['sys-libs/x-2', 'sys-apps/a-2', 'sys-apps/b-2',
 						'sys-apps/e-2'],
 					ignore_mergelist_order = True,
 					success = True),
 
+				ResolverPlaygroundTestCase(
+					["=sys-libs/x-1-r1"],
+					options = {"--rebuild-if-unbuilt" : True},
+					mergelist = ['sys-libs/x-1-r1', 'sys-apps/a-2',
+						'sys-apps/b-2', 'sys-apps/e-2', 'sys-apps/g-2'],
+					ignore_mergelist_order = True,
+					success = True),
+
+				ResolverPlaygroundTestCase(
+					["=sys-libs/x-1-r1"],
+					options = {"--rebuild-if-new-rev" : True},
+					mergelist = ['sys-libs/x-1-r1', 'sys-apps/a-2',
+						'sys-apps/b-2', 'sys-apps/e-2', 'sys-apps/g-2'],
+					ignore_mergelist_order = True,
+					success = True),
+
+				ResolverPlaygroundTestCase(
+					["=sys-libs/x-1-r1"],
+					options = {"--rebuild-if-new-ver" : True},
+					mergelist = ['sys-libs/x-1-r1'],
+					ignore_mergelist_order = True,
+					success = True),
+
+				ResolverPlaygroundTestCase(
+					["sys-libs/x"],
+					options = {"--rebuild-if-new-ver" : True},
+					mergelist = ['sys-libs/x-2', 'sys-apps/a-2',
+						'sys-apps/b-2', 'sys-apps/e-2', 'sys-apps/g-2'],
+					ignore_mergelist_order = True,
+					success = True),
 			)
 
 		playground = ResolverPlayground(ebuilds=ebuilds,
