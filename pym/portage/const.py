352648c70add8cf9881f694f4d044860a088f6a0
Zac Medico <zmedico@gentoo.org>
2011-08-31 10:54:52 -0700

    Add FEATURES=clean-logs support.
    Enable automatic execution of the command specified by the
    PORT_LOGDIR_CLEAN variable. The default PORT_LOGDIR_CLEAN setting will
    remove all files from PORT_LOGDIR that were last modified at least 7
    days ago.
    (cherry picked from commit 377720b203f2f22609da0429ec866ba671fc2da0)
    
    BUG=chromium-os:28874
    TEST=`emerge portage` cleaned out my old log files from /var/log/portage
    
    Change-Id: I99e4f9ca4f50081d3929f215099350f677e89e3c
    Reviewed-on: https://gerrit.chromium.org/gerrit/28050
    Reviewed-by: Brian Harring <ferringb@chromium.org>
    Commit-Ready: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- pym/portage/const.py
+++ pym/portage/const.py
@@ -87,7 +87,8 @@ EBUILD_PHASES            = ("pretend", "setup", "unpack", "prepare", "configure"
                            "nofetch", "config", "info", "other")
 SUPPORTED_FEATURES       = frozenset([
                            "assume-digests", "binpkg-logs", "buildpkg", "buildsyspkg", "candy",
-                           "ccache", "chflags", "collision-protect", "compress-build-logs",
+                           "ccache", "chflags", "clean-logs",
+                           "collision-protect", "compress-build-logs",
                            "digest", "distcc", "distcc-pump", "distlocks", "ebuild-locks", "fakeroot",
                            "fail-clean", "fixpackages", "force-mirror", "getbinpkg",
                            "installsources", "keeptemp", "keepwork", "fixlafiles", "lmirror",
